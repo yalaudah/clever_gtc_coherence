a3b3b775b2b69a557f846a354e4203809552d775
Yazeed Alaudah <yalaudah@gmail.com>
2018-01-31 22:16:00 -0500

    updated the code

--- main.py
+++ main.py
@@ -1,45 +1,53 @@
 
-m __future__ import division
+from __future__ import division
 
 import numpy as np
 from scipy.stats import multivariate_normal
 
-def calc(x, cube_size=3, sigma=2.5):
+def calc(x, cube_size=3, sigma=15):
     # make sure cube_size is odd:
     assert(cube_size%2 != 0)
-    
-    center = np.ceil(cube_size/2)
-    buffer = (cube_size-1)//2 # integer_division
+
+    x = np.array(x)
+    dims = x.shape
+    cube_size = int(cube_size)
+    coherence = np.zeros(x.shape[1:])
+    npad = ((cube_size*2-1-dims[0],0), (cube_size-1,cube_size-1),(cube_size-1, cube_size-1))
+    x = np.pad(x, pad_width=npad, mode='symmetric')
+
+
+    buffer = (cube_size-1)//2
     
     def unfold_tensor(array,mode):
       return np.rollaxis(array,mode,0).reshape(array.shape[mode],-1)
       
-    def gaussian_kernel(cube_size, buffer):
-      x_ = np.linspace(-buffer,buffer,)    
-        
-      
-      multivariate_normal(xyz,mean=0,cov=sigma)
-      
-      G = np.zeros((cube_size,cube_size,cube_size))
+    def gaussian_kernel(cube_size):
+        # center = np.ceil(cube_size / 2)
+        xx, yy, zz = np.mgrid[-1.0:1.0:cube_size*1j, -1.0:1.0:cube_size*1j,-1.0:1.0:cube_size*1j]
+        xyz = np.column_stack([xx.flat, yy.flat, zz.flat])
+        G = multivariate_normal.pdf(xyz, mean=[0,0,0], cov=sigma)
+        G = (2*np.pi*sigma)**(3/2) * G
+        return G.reshape([cube_size,cube_size,cube_size])
 
 
-
-#      for i in xrange(cube_size):
-#        for j in xrange(cube_size):
-#         for k in xrange(cube_size):
-#            G[i,j,k]=np.exp(-0.5*)
-      
-      
-      
-      
-      return G
-    
     CC1 = np.zeros() # should be size of 2D input in x
     CC2 = np.zeros() # should be size of 2D input in x
     CC3 = np.zeros() # should be size of 2D input in x
 
 
-    center = np.ceil(cube_size/2)
+
+    # MUST REARRANGE X HERE
+
+      # G = np.zeros((cube_size,cube_size,cube_size))
+    G = gaussian_kernel(cube_size)
+
+    for i in xrange(1, 10, 2):
+        for j in xrange():
+            for k in xrange():
+
+    for i in xrange(dims[1]):
+        for j in xrange(dims[2]):
+     aCube =
 
 
 
@@ -49,12 +57,8 @@ def calc(x, cube_size=3, sigma=2.5):
 
 
 
-    x = np.array(x)
-    dims = x.shape
-    cube_size = int(cube_size)
-    GoT_value = np.zeros(x.shape[1:])
-    npad = ((cube_size*2-1-dims[0],0), (cube_size-1,cube_size-1),(cube_size-1, cube_size-1))
-    x = np.pad(x, pad_width=npad, mode='symmetric')
+
+
 
     def dissimilarity(x1, x2):
         x1 = x1 - np.mean(x1)
